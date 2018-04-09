ab46fec76790fa80107ba3069f44ea317b3fc37b
cfMMOC for terrain rendering library <35916245+cfmmoc@users.noreply.github.com>
2018-04-09 16:45:09 +0800

    add comment

--- include/bload.h
+++ include/bload.h
@@ -1,23 +1,52 @@
+/**
+    `cfMMOC` library for terrain rendering using OGRE (https://github.com/cfmmoc/cfmmoc/)
+    Licensed under The GNU General Public License v3.0 (GPLv3)
+    Any modification, re-utilization or copy of the source or binary format in other software or publications should mention a CITATION of this library.
+    Copyright (c) 2016-2018 by Authors (Jin Yan, Guanghong Gong, Ni Li and Luhao Xiao)
+**/
+
 #pragma once
 
 #include "Ogre.h"
 using namespace Ogre;
 
+/**
+  @brief  A class for fetch tile from server in back-end thread.
+**/
+
 class BBackLoadThread : public Ogre::ResourceBackgroundQueue::Listener
 {
 public:
-	BBackLoadThread(Ogre::String texExt, Ogre::String meshFD);
+	/**
+    	@remarks
+        	initialize parameters
+    	@par
+		meshFD		folder name for simplified mesh 
+    	**/
+	BBackLoadThread(Ogre::String meshFD);
 	~BBackLoadThread();
 
-	void fireRequestByName(Ogre::String reqname, bool preparation, bool loadTex);
+	/**
+	@remark
+		fire a request to back-end thread
+	@par
+		reqname		file name of a tile
+		preparation	indicate if request is prepared in back-edn thread
+	**/
+	void fireRequestByName(Ogre::String reqname, bool preparation);
 	void operationCompleted(BackgroundProcessTicket ticket, const BackgroundProcessResult& result);
-	unsigned long getMilliseconds();
-	std::vector<Ogre::String> getLoadedFilename(bool loadTex);
+	/**
+	@remark
+		return tiles which is already fetched
+	**/
+	std::vector<Ogre::String> getLoadedFilename();
+	/**
+	@remark
+		return true if fetch queue is empty
+	**/
 	bool isQueueEmpty();
 
 protected:
-	Ogre::Timer mTimer;
-	unsigned long mMilliseconds;
 	unsigned int mLoadType;
 	Ogre::StringVector mLoadList;
 	std::map<Ogre::BackgroundProcessTicket, Ogre::String> mTicketFilename;
