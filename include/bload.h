4be781026fd4a22c38cac316bf942d6618ca3bb4
cfMMOC for terrain rendering library <35916245+cfmmoc@users.noreply.github.com>
2018-04-13 20:32:33 +0800

    Update bload.h

--- include/bload.h
+++ include/bload.h
@@ -11,7 +11,7 @@
 using namespace Ogre;
 
 /**
-  @brief  A class for fetching tile from server for back-end thread.
+  @brief  A class for fetching tiles from server for back-end process.
 **/
 
 class BBackLoadThread : public Ogre::ResourceBackgroundQueue::Listener
